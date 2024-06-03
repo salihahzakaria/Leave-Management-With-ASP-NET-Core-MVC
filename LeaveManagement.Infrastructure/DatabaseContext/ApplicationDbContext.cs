ded7cb328389f8ae8196f4ca49f35728648df39a
Saliha <saliha.work@gmail.com>
2024-06-03 11:05:04 +0800

    - Implemented ApplicationDbContext class - Add connection string

---
+++ LeaveManagement.Infrastructure/DatabaseContext/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using LeaveManagement.Core.Domain.Entities;
+using LeaveManagement.Core.Domain.IdentityEntities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeaveManagement.Infrastructure.DatabaseContext
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public virtual DbSet<LeaveType> LeaveType { get; set; }
+        public virtual DbSet<Leave> Leave { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<LeaveType>().ToTable("LeaveType");
+            modelBuilder.Entity<Leave>().ToTable("Leave");
+
+            //Seed data to leavetype
+            string leavetypesJson = File.ReadAllText("leavetypes.json");
+            List<LeaveType> leaveTypes = System.Text.Json.JsonSerializer.Deserialize<List<LeaveType>>(leavetypesJson);
+
+            foreach (LeaveType leaveType in leaveTypes)
+            {
+                modelBuilder.Entity<LeaveType>().HasData(leaveType);
+            }
+        }
+    }
+}
--- LeaveManagement.UI/Program.cs
+++ LeaveManagement.UI/Program.cs
@@ -1,6 +1,16 @@
+using LeaveManagement.Infrastructure.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    });
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
 
 app.Run();
