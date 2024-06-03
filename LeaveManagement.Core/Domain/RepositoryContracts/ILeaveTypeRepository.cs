61b52b4e8a1d284c52ad7dd236c1ab9b056a654f
Saliha <saliha.work@gmail.com>
2024-06-03 11:18:42 +0800

    Creating interface for leave type repository

---
+++ LeaveManagement.Core/Domain/RepositoryContracts/ILeaveTypeRepository.cs
@@ -0,0 +1,15 @@
+﻿using LeaveManagement.Core.Domain.Entities;
+
+namespace LeaveManagement.Core.Domain.RepositoryContracts
+{
+    public interface ILeaveTypeRepository
+    {
+        Task<LeaveType> AddLeaveType(LeaveType leaveType);
+
+        Task<List<LeaveType>> GetAllLeavesType();
+
+        Task<LeaveType?> GetLeaveTypeByID(Guid leaveTypeID);
+
+        Task<LeaveType?> GetLeaveTypeByName(string leaveTypeName);
+    }
+}
