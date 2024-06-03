679931d068f2d6e599964e1202e26bb0ce29ade7
Saliha <saliha.work@gmail.com>
2024-06-03 11:23:47 +0800

    Creating leave repository class and implement the interface

---
+++ LeaveManagement.Infrastructure/Repositories/LeaveRepository.cs
@@ -0,0 +1,44 @@
+﻿using LeaveManagement.Core.Domain.Entities;
+using LeaveManagement.Core.Domain.RepositoryContracts;
+using System.Linq.Expressions;
+
+namespace LeaveManagement.Infrastructure.Repositories
+{
+    public class LeaveRepository : ILeaveRepository
+    {
+        public Task<Leave> AddLeave(Leave leave)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteLeave(Guid leaveID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Leave>> GetAllLeaves()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Leave>> GetFilteredLeaves(Expression<Func<Leave, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Leave?> GetLeaveByLeaveID(Guid leaveID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Leave>> GetLeaveByUserID(Guid? userID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Leave> UpdateLeave(Leave leave)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
