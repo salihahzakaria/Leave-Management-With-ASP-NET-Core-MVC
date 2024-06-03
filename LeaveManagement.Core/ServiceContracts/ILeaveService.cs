d44c84fe86e9cc9f8e1747e972c917b5d181c34c
Saliha <saliha.work@gmail.com>
2024-06-03 15:28:30 +0800

    Creating interface of LeaveService

---
+++ LeaveManagement.Core/ServiceContracts/ILeaveService.cs
@@ -0,0 +1,24 @@
+﻿using LeaveManagement.Core.DTO;
+using LeaveManagement.Core.Enums;
+
+namespace LeaveManagement.Core.ServiceContracts
+{
+    public interface ILeaveService
+    {
+        Task<LeaveResponse> AddLeave(LeaveAddRequest? leaveAddRequest);
+        Task<List<LeaveResponse>> GetAllLeaves();
+
+        Task<LeaveResponse?> GetLeaveByLeaveID(Guid? leaveID);
+
+        Task<List<LeaveResponse>> GetLeaveByUserID(Guid? userID);
+
+        Task<List<LeaveResponse>> GetFilteredLeaves(string searchBy, string? searchString);
+
+        Task<LeaveResponse> UpdateLeave(LeaveUpdateRequest? leaveUpdateRequest);
+
+        Task<bool> DeleteLeave(Guid? leaveID);
+
+        Task<List<LeaveResponse>> GetSortedLeaves(List<LeaveResponse> allLeaves, string sortBy, SortOrderOptions sortOrderOptions);
+
+    }
+}
