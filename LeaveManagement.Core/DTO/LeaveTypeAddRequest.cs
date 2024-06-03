5c143f2716cf26d6cde0fecbe6d446749841a0db
Saliha <saliha.work@gmail.com>
2024-06-03 15:06:06 +0800

    Adding DTO class for leave type

---
+++ LeaveManagement.Core/DTO/LeaveTypeAddRequest.cs
@@ -0,0 +1,20 @@
+﻿using LeaveManagement.Core.Domain.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.Core.DTO
+{
+    public class LeaveTypeAddRequest
+    {
+        [Required(ErrorMessage = "{0} cannot be blank")]
+        [Display(Name = "Leave Type")]
+        public string? Name { get; set; }
+
+        public LeaveType ToLeaveType()
+        {
+            return new LeaveType()
+            {
+                Name = Name
+            };
+        }
+    }
+}
---
+++ LeaveManagement.Core/DTO/LeaveTypeResponse.cs
@@ -0,0 +1,50 @@
+﻿using LeaveManagement.Core.Domain.Entities;
+
+namespace LeaveManagement.Core.DTO
+{
+    public class LeaveTypeResponse
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            if (obj.GetType() != typeof(LeaveTypeResponse))
+            {
+                return false;
+            }
+
+            LeaveTypeResponse leaveType_to_compare = (LeaveTypeResponse)obj;
+
+            return Id == leaveType_to_compare.Id &&
+                Name == leaveType_to_compare.Name;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"Leave Type ID: {Id}, Name: {Name}";
+        }
+    }
+
+    public static class LeaveTypeExtensions
+    {
+        public static LeaveTypeResponse ToLeaveTypeResponse(this LeaveType leaveType)
+        {
+            return new LeaveTypeResponse
+            {
+                Id = leaveType.Id,
+                Name = leaveType.Name
+            };
+        }
+    }
+}
