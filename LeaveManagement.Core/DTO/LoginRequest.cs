6c0a59398c4b4bc5d266f22925483b823bc0f387
Saliha <saliha.work@gmail.com>
2024-06-03 15:19:42 +0800

    Adding DTO for Login

---
+++ LeaveManagement.Core/DTO/LoginRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.Core.DTO
+{
+    public class LoginRequest
+    {
+        [Required(ErrorMessage = "{0} cannot be blank")]
+        [EmailAddress(ErrorMessage = "{0} should be in a valid email address format")]
+        [DataType(DataType.EmailAddress)]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "{0} cannot be blank")]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+    }
+}
