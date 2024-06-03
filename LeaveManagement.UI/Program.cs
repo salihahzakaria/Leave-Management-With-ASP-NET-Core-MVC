a3b567e7eb4b19376f412ee275428a53d2e50d54
Saliha <saliha.work@gmail.com>
2024-06-03 11:08:25 +0800

    - Adding identity as services

--- LeaveManagement.UI/Program.cs
+++ LeaveManagement.UI/Program.cs
@@ -1,4 +1,7 @@
+using LeaveManagement.Core.Domain.IdentityEntities;
 using LeaveManagement.Infrastructure.DatabaseContext;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +12,21 @@ builder.Services.AddDbContext<ApplicationDbContext>(
     {
         options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
     });
+
+builder.Services
+    .AddIdentity<ApplicationUser, ApplicationRole>(options =>
+    {
+        options.Password.RequiredLength = 8;
+        options.Password.RequireNonAlphanumeric = true;
+        options.Password.RequireUppercase = true;
+        options.Password.RequireLowercase = true;
+        options.Password.RequireDigit = true;
+        options.Password.RequiredUniqueChars = 5;    //Eg: Abcbc1!#
+    })
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders()
+    .AddUserStore<UserStore<ApplicationUser, ApplicationRole, ApplicationDbContext, Guid>>()
+    .AddRoleStore<RoleStore<ApplicationRole, ApplicationDbContext, Guid>>();
 
 var app = builder.Build();
 
