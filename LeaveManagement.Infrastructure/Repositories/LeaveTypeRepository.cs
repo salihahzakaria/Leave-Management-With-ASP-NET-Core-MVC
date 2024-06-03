4aaa3149a0910319a8ecb8f76a9d648051cc09c6
Saliha <saliha.work@gmail.com>
2024-06-03 11:22:32 +0800

    Creating leave type repository class and implement the interface

---
+++ LeaveManagement.Infrastructure/Repositories/LeaveTypeRepository.cs
@@ -0,0 +1,28 @@
+﻿using LeaveManagement.Core.Domain.Entities;
+using LeaveManagement.Core.Domain.RepositoryContracts;
+
+namespace LeaveManagement.Infrastructure.Repositories
+{
+    public class LeaveTypeRepository : ILeaveTypeRepository
+    {
+        public Task<LeaveType> AddLeaveType(LeaveType leaveType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<LeaveType>> GetAllLeavesType()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LeaveType?> GetLeaveTypeByID(Guid leaveTypeID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LeaveType?> GetLeaveTypeByName(string leaveTypeName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
