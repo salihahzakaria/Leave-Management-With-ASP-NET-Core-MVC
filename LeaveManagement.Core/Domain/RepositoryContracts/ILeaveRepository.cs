cb9fb75803f47b5c7acc33e0d0f15fdcae916dbf
Saliha <saliha.work@gmail.com>
2024-06-03 11:20:07 +0800

    Creating interface for leave repository

---
+++ LeaveManagement.Core/Domain/RepositoryContracts/ILeaveRepository.cs
@@ -0,0 +1,21 @@
+﻿using LeaveManagement.Core.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace LeaveManagement.Core.Domain.RepositoryContracts
+{
+    public interface ILeaveRepository
+    {
+        Task<Leave> AddLeave(Leave leave);
+
+        Task<List<Leave>> GetAllLeaves();
+
+        Task<Leave?> GetLeaveByLeaveID(Guid leaveID);
+        Task<List<Leave>> GetLeaveByUserID(Guid? userID);
+
+        Task<List<Leave>> GetFilteredLeaves(Expression<Func<Leave, bool>> predicate);
+
+        Task<Leave> UpdateLeave(Leave leave);
+
+        Task<bool> DeleteLeave(Guid leaveID);
+    }
+}
