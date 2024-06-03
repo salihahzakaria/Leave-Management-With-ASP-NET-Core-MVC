fb51ebfbba607fcacac0b5e6188ad9037b69a5a8
Saliha <saliha.work@gmail.com>
2024-06-03 15:31:08 +0800

    Creating Leave type services class and implement the interface

---
+++ LeaveManagement.Core/Services/LeaveTypeService.cs
@@ -0,0 +1,23 @@
+﻿using LeaveManagement.Core.DTO;
+using LeaveManagement.Core.ServiceContracts;
+
+namespace LeaveManagement.Core.Services
+{
+    public class LeaveTypeService : ILeaveTypeService
+    {
+        public Task<LeaveTypeResponse> AddLeaveType(LeaveTypeAddRequest? leaveTypeAddRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<LeaveTypeResponse>> GetAllLeavesType()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LeaveTypeResponse?> GetLeaveTypeByLeaveTypeID(Guid? leaveTypeID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
