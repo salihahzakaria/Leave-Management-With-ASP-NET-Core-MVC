1fcf11e5d8a7caf360ca55b942772e7da39b38c5
Saliha <saliha.work@gmail.com>
2024-06-03 15:41:06 +0800

    Add ValidationHelper for model validation

---
+++ LeaveManagement.Core/Helpers/ValidationHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.Core.Helpers
+{
+    public class ValidationHelper
+    {
+        public static void ModelValidation(object obj)
+        {
+            ValidationContext validationContext = new ValidationContext(obj);
+            List<ValidationResult> validationResults = new List<ValidationResult>();
+
+            bool isValid = Validator.TryValidateObject(obj, validationContext, validationResults, true);
+
+            if (!isValid)
+            {
+                throw new ArgumentException(validationResults.FirstOrDefault()?.ErrorMessage);
+            }
+        }
+    }
+}
