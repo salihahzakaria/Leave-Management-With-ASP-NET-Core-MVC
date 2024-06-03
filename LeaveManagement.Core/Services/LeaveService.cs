a83acbfe33d0c0a9bfa8aa121f39d91c108660c0
Saliha <saliha.work@gmail.com>
2024-06-03 15:32:21 +0800

    Creating Leave service class and implement the interface

---
+++ LeaveManagement.Core/Services/LeaveService.cs
@@ -0,0 +1,49 @@
+﻿using LeaveManagement.Core.DTO;
+using LeaveManagement.Core.Enums;
+using LeaveManagement.Core.ServiceContracts;
+
+namespace LeaveManagement.Core.Services
+{
+    public class LeaveService : ILeaveService
+    {
+        public Task<LeaveResponse> AddLeave(LeaveAddRequest? leaveAddRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteLeave(Guid? leaveID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<LeaveResponse>> GetAllLeaves()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<LeaveResponse>> GetFilteredLeaves(string searchBy, string? searchString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LeaveResponse?> GetLeaveByLeaveID(Guid? leaveID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<LeaveResponse>> GetLeaveByUserID(Guid? userID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<LeaveResponse>> GetSortedLeaves(List<LeaveResponse> allLeaves, string sortBy, SortOrderOptions sortOrderOptions)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LeaveResponse> UpdateLeave(LeaveUpdateRequest? leaveUpdateRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
