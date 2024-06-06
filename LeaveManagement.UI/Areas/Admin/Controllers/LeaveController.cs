6b79afc5dba9a13eb12adcde7efa34d74ddfac0f
Saliha <saliha.work@gmail.com>
2024-06-06 18:46:42 +0800

    Implement AccountController with Edit method

--- LeaveManagement.UI/Areas/Admin/Controllers/LeaveController.cs
+++ LeaveManagement.UI/Areas/Admin/Controllers/LeaveController.cs
@@ -1,5 +1,6 @@
 ﻿using LeaveManagement.Core.Domain.IdentityEntities;
 using LeaveManagement.Core.DTO;
+using LeaveManagement.Core.Enums;
 using LeaveManagement.Core.ServiceContracts;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -85,6 +86,94 @@ namespace LeaveManagement.UI.Areas.Admin.Controllers
             }
 
             LeaveResponse leaveResponse = await _leaveService.AddLeave(leaveAddRequest);
+
+            return RedirectToAction("Index", "Leave", new
+            {
+                area = "Admin"
+            });
+        }
+
+        [Route("[action]/{leaveId}")]
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid leaveID)
+        {
+            LeaveResponse? leaveResponse = await _leaveService.GetLeaveByLeaveID(leaveID);
+
+            if (leaveResponse == null)
+            {
+                return RedirectToAction("Index", "Leave", new
+                {
+                    area = "Admin"
+                });
+            }
+
+            LeaveUpdateRequest leaveUpdateRequest = leaveResponse.ToLeaveUpdateRequest();
+
+            List<LeaveTypeResponse> leaveTypes = await _leaveTypeService.GetAllLeavesType();
+            ViewBag.LeaveTypes = leaveTypes.Select(temp => new SelectListItem()
+            {
+                Text = temp.Name,
+                Value = temp.Id.ToString()
+            });
+
+            List<ApplicationUser> users = await _userManager.Users.ToListAsync();
+            ViewBag.Users = users.Select(temp => new SelectListItem()
+            {
+                Text = temp.EmployeeName,
+                Value = temp.Id.ToString()
+            });
+
+            List<StatusOptions> statuses = Enum.GetValues(typeof(StatusOptions)).Cast<StatusOptions>().ToList();
+            ViewBag.Statuses = statuses.Select(temp => new SelectListItem()
+            {
+                Text = temp.ToString(),
+                Value = temp.ToString()
+            });
+
+            ApplicationUser approver = await _userManager.GetUserAsync(User);
+            ViewBag.ApproverID = approver.Id;
+            ViewBag.Approver = approver.EmployeeName;
+
+            return View(leaveUpdateRequest);
+        }
+
+        [Route("[action]/{leaveId}")]
+        [HttpPost]
+        public async Task<IActionResult> Edit(LeaveUpdateRequest leaveUpdateRequest)
+        {
+            LeaveResponse? leaveResponse = await _leaveService.GetLeaveByLeaveID(leaveUpdateRequest.Id);
+
+            if (leaveResponse == null)
+            {
+                return RedirectToAction("Index", "Leaves");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                List<LeaveTypeResponse> leaveTypes = await _leaveTypeService.GetAllLeavesType();
+                ViewBag.LeaveTypes = leaveTypes.Select(temp => new SelectListItem()
+                {
+                    Text = temp.Name,
+                    Value = temp.Id.ToString()
+                });
+
+                List<ApplicationUser> users = await _userManager.Users.ToListAsync();
+                ViewBag.Users = users.Select(temp => new SelectListItem()
+                {
+                    Text = temp.EmployeeName,
+                    Value = temp.Id.ToString()
+                });
+
+                ApplicationUser approver = await _userManager.GetUserAsync(User);
+                ViewBag.ApproverID = approver.Id;
+                ViewBag.Approver = approver.EmployeeName;
+
+                ViewBag.Errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage).ToList();
+
+                return View(leaveResponse.ToLeaveUpdateRequest());
+            }
+
+            LeaveResponse updatedLeave = await _leaveService.UpdateLeave(leaveUpdateRequest);
 
             return RedirectToAction("Index", "Leave", new
             {
